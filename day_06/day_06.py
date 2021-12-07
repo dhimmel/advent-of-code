e71bf5952dd31c8897bb3ff2c23f82c0ca21c1c3
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2021-12-06 21:33:55 -0800

    pre-commit updates

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -2,11 +2,12 @@
 Day 6: Lanternfish
 https://adventofcode.com/2021/day/6
 """
-from pathlib import Path
 from collections import Counter
+from pathlib import Path
 
 n_times = 9
 """Total number of day/time states a fish can exist in"""
+
 
 def read_input(test=True) -> list[int]:
     path = "test_input.txt" if test else "input.txt"
@@ -14,21 +15,24 @@ def read_input(test=True) -> list[int]:
     counts = Counter(int(x) for x in text.split(","))
     return [counts[i] for i in range(n_times)]
 
-def increment(counts: list[int])-> list[int]:
+
+def increment(counts: list[int]) -> list[int]:
     new_counts = n_times * [0]
     for i in range(1, n_times):
-        new_counts[i-1] = counts[i]
+        new_counts[i - 1] = counts[i]
     # lanternfish that creates a new fish resets its timer to 6
     new_counts[6] += counts[0]
     # new lanternfish starts with an internal timer of 8
     new_counts[n_times - 1] += counts[0]
     return new_counts
 
+
 def population_size(days: int, test: bool) -> int:
     counts = read_input(test=test)
     for _ in range(days):
         counts = increment(counts)
     return sum(counts)
+
 
 assert population_size(days=18, test=True) == 26
 assert population_size(days=80, test=True) == 5934
--- day_07/day_07.py
+++ day_07/day_07.py
@@ -2,21 +2,24 @@
 Day 7: The Treachery of Whales
 https://adventofcode.com/2021/day/7
 """
-from pathlib import Path
 from math import comb
+from pathlib import Path
 
-def read_input(test: bool=True) -> list[int]:
+
+def read_input(test: bool = True) -> list[int]:
     path = "test_input.txt" if test else "input.txt"
     text = Path(path).read_text().strip()
     return [int(x) for x in text.split(",")]
 
-def compute_fuel(n_moves: int, constant: bool=True) -> int:
+
+def compute_fuel(n_moves: int, constant: bool = True) -> int:
     if constant:
         return abs(n_moves)
     # crab submarine engines don't burn fuel at a constant rate
     return comb(abs(n_moves) + 1, 2)
 
-def get_min_fuel(test: bool=True, constant: bool=True) -> int:
+
+def get_min_fuel(test: bool = True, constant: bool = True) -> int:
     positions = read_input(test=test)
     scores = list()
     for i in range(min(positions), max(positions) + 1):
@@ -24,6 +27,7 @@ def get_min_fuel(test: bool=True, constant: bool=True) -> int:
         scores.append((score, i))
     fuel, position = min(scores)
     return fuel
+
 
 assert get_min_fuel(test=True, constant=True) == 37
 assert get_min_fuel(test=True, constant=False) == 168
